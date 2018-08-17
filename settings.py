f3f3d578dfd76176dcf1d56707b846dac0cb54e6
johnt1000 <jonatas.rodriguess@gmail.com>
2018-08-17 14:14:32 -0300

    Primeira implementação da entidade Papel do sistema. Issue #1

--- settings.py
+++ settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'papeis.apps.PapeisConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
