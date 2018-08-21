09abb60460fbb6b51364830ec1037436f89e384b
johnt1000 <jonatas.rodriguess@gmail.com>
2018-08-21 11:47:28 -0300

    Removido código comentado da aplicação Papeis.

--- papeis/urls.py
+++ papeis/urls.py
@@ -13,7 +13,4 @@ schema_view = get_swagger_view(title='Papeis API')
 urlpatterns = [
     url(r'^$', schema_view),
     url(r'^', include(router.urls)),
-    # path('', views.index, name='index'),
-    # url(r'^api-v1/', include(router.urls, 'papeis')),
-    # url(r'^api-v1/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- papeis/views.py
+++ papeis/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 from papeis.models import Papel
 from rest_framework import viewsets, mixins
 from papeis.serializers import PapelSerializer
 
-
-# def index(request):
-#     return HttpResponse("Hello, world. You're at the polls index.")
 
 # mixins.CreateModelMixin, 
 # mixins.RetrieveModelMixin, # retorna unico registro
