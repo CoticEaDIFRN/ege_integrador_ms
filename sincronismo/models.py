a2b42eddb44a42cbf60f86d5e84a1d5ab930501a
johnt1000 <jonatas.rodriguess@gmail.com>
2018-08-22 15:17:37 -0300

    Implementada base para acessos a API Moodle e SUAP. closed #16

--- sincronismo/models.py
+++ sincronismo/models.py
@@ -4,10 +4,10 @@ import json, requests
 
 class Base(object):
     
-    def __init__(self, urlBase, token, resource):
+    def __init__(self, urlBase, token):
+        # super(Base, self).__init__()
         self._urlBase = urlBase
         self._token = token
-        self._resource = resource
     
     def getUrlBase(self):
         return self._urlBase
@@ -27,13 +27,34 @@ class Base(object):
     def setResource(self, resource):
         self._resource = resource
 
+    def getResponseFormat(self):
+        return self._responseFormat
+    
+    def setResponseFormat(self, responseFormat):
+        self._responseFormat = responseFormat
+
+
 class Moodle(Base):
 
-    def __init__(self, urlBase, token, resource):
-        super(Moodle, self).__init__(urlBase, token, resource)
+    def __init__(self):
+        # TODO Colocar em arquivo de configuração da app
+        token = "0b0c9af5bd3eba5a6fccbc3d1594376f"
+        urlBase = 'http://localhost:8080/moodle/webservice/rest/server.php'
+        responseFormat = 'json'
+
+        super(Moodle, self).__init__(urlBase, token)
+        self.setResponseFormat(responseFormat)
+
 
 class Suap(Base):
     
-    def __init__(self, urlBase, token, resource):
-        super(Suap, self).__init__(urlBase, token, resource)
+    def __init__(self):
+        # TODO Colocar em arquivo de configuração da app
+        token = "0b0c9af5bd3eba5a6fccbc3d1594376f"
+        urlBase = 'http://localhost:8080/moodle/webservice/rest/server.php'
+        responseFormat = 'json'
+
+        super(Suap, self).__init__(urlBase, token)
+        self.setResponseFormat(responseFormat)
+
     
--- sincronismo/tests.py
+++ sincronismo/tests.py
@@ -2,20 +2,40 @@ from django.test import TestCase
 
 from .models import Base
 from .models import Moodle
+from .models import Suap
+
+import json, requests
 
 class BaseModelTests(TestCase):
     
     # def setUp(self):
     #     base = Base('http://localhost:8000', 'abcdefghijlmnopqrstuvxz', 'get_users')
 
-    def teste_de_sanidade(self):
+    def test_de_sanidade(self):
         """
         Verifica se o objeto tem o comportamento padrão esperado.
         """
-        base = Base('http://localhost:8000', 'abcdefghijlmnopqrstuvxz', 'get_users')
+        base = Base('http://localhost:8000', 'abcdefghijlmnopqrstuvxz')
         self.assertEqual(base.getUrlBase(), 'http://localhost:8000')
         self.assertEqual(base.getToken(), 'abcdefghijlmnopqrstuvxz')
+    
+    def test_resource(self):
+        """
+        Verifica se o objeto tem o comportamento esperado quando atribuido
+        algum recurso a ele.
+        """
+        base = Base('http://localhost', 'test')
+        base.setResource('get_users')
         self.assertEqual(base.getResource(), 'get_users')
+    
+    def test_responseFormat(self):
+        """
+        Verifica se o objeto tem o comportamento esperado quando atribuido
+        algum formato de resposta a ele.
+        """
+        base = Base('http://localhost', 'test')
+        base.setResponseFormat('json')
+        self.assertEqual(base.getResponseFormat(), 'json')
 
 class MoodleModelTests(TestCase):
 
@@ -23,10 +43,10 @@ class MoodleModelTests(TestCase):
         """
         Verifica se o objeto tem o comportamento padrão esperado.
         """
-        model = Moodle('http://localhost:8000', 'abcdefghijlmnopqrstuvxz', 'get_users')
-        self.assertEqual(model.getUrlBase(), 'http://localhost:8000')
-        self.assertEqual(model.getToken(), 'abcdefghijlmnopqrstuvxz')
-        self.assertEqual(model.getResource(), 'get_users')
+        model = Moodle()
+        self.assertEqual(model.getUrlBase(), 'http://localhost:8080/moodle/webservice/rest/server.php')
+        self.assertEqual(model.getToken(), '0b0c9af5bd3eba5a6fccbc3d1594376f')
+        self.assertEqual(model.getResponseFormat(), 'json')
 
 class SuapModelTests(TestCase):
     
@@ -34,7 +54,7 @@ class SuapModelTests(TestCase):
         """
         Verifica se o objeto tem o comportamento padrão esperado.
         """
-        model = Moodle('http://localhost:8000', 'abcdefghijlmnopqrstuvxz', 'get_users')
-        self.assertEqual(model.getUrlBase(), 'http://localhost:8000')
-        self.assertEqual(model.getToken(), 'abcdefghijlmnopqrstuvxz')
-        self.assertEqual(model.getResource(), 'get_users')
+        model = Suap()
+        self.assertEqual(model.getUrlBase(), 'http://localhost:8080/moodle/webservice/rest/server.php')
+        self.assertEqual(model.getToken(), '0b0c9af5bd3eba5a6fccbc3d1594376f')
+        self.assertEqual(model.getResponseFormat(), 'json')
